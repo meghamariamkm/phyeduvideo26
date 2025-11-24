b21bbb1342b8c9a68d5035aef9f5072b5a6974db
meghamariamkm2002 <megha.km@research.iiit.ac.in>
2025-11-24 23:25:21 +0530

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   base: '/phyeduvideo26/',  // <-- Add this line for GitHub Pages
+   plugins: [react()],
 })
